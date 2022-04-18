c0bd3c94db22477e6e8c0e38095a0dc6ba303cd2
rajia124 <rajia5203@gmail.com>
2022-04-18 13:41:52 +0600

    readme done

--- src/Pages/Notfound/Notfound.js
+++ src/Pages/Notfound/Notfound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Notfound = () => {
   return (
     <div>
       <h1>404 Error</h1>
+      <button><Link to={'/'}>Back To Home</Link></button>
     </div>
   );
 };
