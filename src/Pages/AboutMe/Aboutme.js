bd93e8a61a39980d7ea9756af1346eed1d1d1485
rajia124 <rajia5203@gmail.com>
2022-04-18 13:21:58 +0600

    about

--- src/Pages/AboutMe/Aboutme.js
+++ src/Pages/AboutMe/Aboutme.js
@@ -6,7 +6,7 @@ const Aboutme = () => {
 
       <div className='d-flex justify-content-center align-item-center mx-auto container'>
         <div className='p-4'><h1 className=' fw-bold'>About Me</h1>
-          <p >My name is MD Habibur Rahman Shuvo .I am from Dhaka .My Goal Be a Full Stack Developer though i have some interest in App developing .Hoping i will be great Developer and thinking for make some unique idea and creative </p></div>
+          <p >My name is MD Habibur Rahman Shuvo .I am from Dhaka .My Goal Be a Full Stack Developer though i have some interest in App developing .Hoping i will be great Developer and thinking for make some unique idea and creative .Trying to best way learning programming  </p></div>
         <div><img className='img-fluid w-100' src='me.jpg' alt="" /></div>
       </div>
     </div>
